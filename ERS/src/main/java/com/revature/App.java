118fbab8f4d157474346af8afd670809e8691415
Fredrick Lindsey <fredricklindsey150@yahoo.com>
2019-03-05 19:08:47 -0600

    testing

--- ERS/src/main/java/com/revature/App.java
+++ ERS/src/main/java/com/revature/App.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.Scanner;
+
 
 public class App 
 {
@@ -15,32 +17,20 @@ public class App
             BufferedReader br = new BufferedReader(isr)
         ) {
             userRequest = br.readLine();
+            Scanner scan = new Scanner(userRequest);
+            scan.useDelimiter(", ");
+            String id = scan.next();
+            String money = scan.next();
+            String manager = scan.next();
+            System.out.println(id+" "+money+" "+manager);
+            String username = br.readLine();
+            String password = br.readLine();
+            StringBuilder passwordHidden = new StringBuilder();
+            for(int i = 0; i<password.length() ; i++){
+                passworHidden.append('*');
+            }
         } catch (IOException exception) {
             exception.printStackTrace();
         }
-        // if(userRequest.equals("login")){
-        //     try(
-        //         InputStreamReader isr = new InputStreamReader(System.in);
-        //         BufferedReader br = new BufferedReader(isr)
-        //     ) {
-        //         System.out.println("Success");
-        //         String loginInput = br.readLine();
-
-        //     } catch (IOException exception) {
-        //         exception.printStackTrace();
-        //     }
-        // }else if(userRequest.equals("new user")){
-        //     try(
-        //         InputStreamReader isr2 = new InputStreamReader(System.in);
-        //         BufferedReader br2 = new BufferedReader(isr2)
-        //     ) {
-        //         String newUser = br2.readLine();
-        //         try (PrintWriter out = new PrintWriter("users.txt")) {
-        //             out.println(newUser);
-        //         }
-        //     } catch (IOException exception) {
-        //         exception.printStackTrace();
-        //     }
-        // }
     }
 }
